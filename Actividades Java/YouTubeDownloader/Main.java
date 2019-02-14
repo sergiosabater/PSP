971768abcae75a16ca25bce14b1f3a3e27e786a6
Sergio Sabater <43619100+sergiosabater@users.noreply.github.com>
2019-02-14 23:38:06 +0100

    Create Main.java

---
+++ Actividades Java/YouTubeDownloader/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.net.URL;
+import com.github.axet.vget.VGet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Interfaz frame = new Interfaz();
+		frame.setVisible(true);
+	}
+}
